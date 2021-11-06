50707906e8049f0618857a1345f49f8505426894
Rickson Lima <lima.rickson20@gmail.com>
2021-11-06 01:05:54 -0400

    import purchase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { PurchaseModule } from './purchase/purchase.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, PurchaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
